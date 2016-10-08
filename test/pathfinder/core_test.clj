1b73ad373a1e5419f27541138a7a4ed1ead4a269
Aleksandr Sher <aleksandrsher@gmail.com>
2016-10-08 22:26:01 +0300

    refactoring

--- test/pathfinder/core_test.clj
+++ test/pathfinder/core_test.clj
@@ -40,20 +40,28 @@
   (t/trace-env)
   (some-inner-fn a b 3))
 
+(defn assert-threads-tracks
+  [n generate-threads-fn]
+  (reset! db [])
+  (generate-threads-fn n)
+  (let [tracks-by-path (group-by :path-id @db)]
+    ;; in every path there is 2 tracks (for two fns)
+    (is (->>
+         tracks-by-path
+         vals
+         (map count)
+         (every? #(= % 2))))
+    ;; there is N track groups (aka paths)
+    (is (= n (count tracks-by-path)))))
+
+(defn generate-futures
+  [n]
+  (let [fs (mapv
+            (fn [_] (future (test-fn 1 2)))
+            (range n))]
+    (while (not (every? realized? fs))
+      (Thread/sleep 100))))
+
 (deftest future-threads-test
   (testing "N futures should produce N different paths"
-    (let [n 10
-          futures (map
-                   (fn [_] (future (test-fn 1 2)))
-                   (range n))]
-      (while (not (every? realized? futures))
-        (Thread/sleep 100))
-      (let [tracks-by-path (group-by :path-id @db)]
-        ;; in every path there is 2 tracks (for two fns)
-        (is (->>
-             tracks-by-path
-             vals
-             (map count)
-             (every? #(= % 2))))
-        ;; there is N track groups (aka paths)
-        (is (= n (count tracks-by-path)))))))
+    (assert-threads-tracks 20 generate-futures)))
