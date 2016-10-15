baa4df450c127380ebf0828cfd745d90458d1226
Aleksandr Sher <aleksandrsher@gmail.com>
2016-10-15 17:21:45 +0300

    + cider for cljs

--- ui/README.md
+++ ui/README.md
@@ -4,6 +4,16 @@ A [re-frame](https://github.com/Day8/re-frame) application designed to ... well,
 
 ## Development Mode
 
+### Start Cider from Emacs:
+
+Put this in your Emacs config file:
+
+```
+(setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
+```
+
+Navigate to a clojurescript file and start a figwheel REPL with `cider-jack-in-clojurescript` or (`C-c M-J`)
+
 ### Run application:
 
 ```
--- ui/project.clj
+++ ui/project.clj
@@ -14,11 +14,16 @@
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.8.2"]]
+   {:dependencies [[binaryage/devtools "0.8.2"]
+                   [figwheel-sidecar "0.5.7"]
+                   [com.cemerick/piggieback "0.2.1"]]
 
-    :plugins      [[lein-figwheel "0.5.7"]]
+    :plugins      [[lein-figwheel "0.5.7"]
+                   [cider/cider-nrepl "0.13.0"]]
     }}
 
   :cljsbuild
